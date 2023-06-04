6e20db2a4468ec89529391911cdaa9fe821c3cc9
Jay Ponkia <ponkiajay19@gmail.com>
2023-06-04 12:03:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App p-3 m-0 border-0 bd-example m-0 border-0 bd-example-row">
+      <div class="container text-center">
+        <div class="row">
+          <div class="col">Id</div>
+          <div class="col">Name</div>
+          <div class="col">PhoneNo</div>
+          <div class="col">Email Id</div>
+          <div class="col">Company Name</div>
+          <div class="col">Action</div>
+        </div>
+        <div class="row">
+          <div class="col">1</div>
+          <div class="col">Leanne Graham</div>
+          <div class="col">1-770-736-8031 x56442</div>
+          <div class="col">Sincere@april.biz</div>
+          <div class="col">Romaguera-Crona</div>
+          <div class="col">Action</div>
+        </div>
+        <div class="row">
+          <div class="col">2</div>
+          <div class="col">Ervin Howell</div>
+          <div class="col">010-692-6593 x09125</div>
+          <div class="col">Shanna@melissa.tv</div>
+          <div class="col">Deckow-Crist</div>
+          <div class="col">Action</div>
+        </div>
+      </div>
     </div>
   );
 }
